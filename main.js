3717aa40bf92b2a9c33076a6471fc43e72e8aa5e
MD. Muztahidur Rahman Nihal <128363204+muztahidur@users.noreply.github.com>
2025-04-29 08:38:31 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadFile('www/index.html'); // আপনার Cordova অ্যাপের ফাইল
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
